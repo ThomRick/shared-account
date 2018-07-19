eb16878d64a8d3ad5644e7c1427613ad9798ac76
thomrick <thomasr2309@gmail.com>
2018-07-19 12:39:59 +0200

    design() : remove in memory repository implementation because it's wrong

--- src/framework/infrastructure/interfaces/repository-base.interface.ts
+++ src/framework/infrastructure/interfaces/repository-base.interface.ts
@@ -1,6 +1,6 @@
 import { IEventBase } from '../../events';
 
 export interface IRepositoryBase {
-  insert(key: string, events: IEventBase[]): Promise<boolean>;
-  find(query: (entity: any) => boolean): Promise<IEventBase[]>;
+  insert(id: string, events: IEventBase[]): Promise<void>;
+  find(): Promise<void>;
 }
