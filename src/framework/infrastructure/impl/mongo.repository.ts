97647a96a109185074eb3d8e1cfcae8ffaf2bda4
thomrick <thomasr2309@gmail.com>
2018-07-21 13:03:40 +0200

    feature(account) : install mongoose and develop a mongo repository impl

---
+++ src/framework/infrastructure/impl/mongo.repository.ts
@@ -0,0 +1,43 @@
+import { IRepository, IDocument } from '../interfaces';
+import { IAggregate } from '../../aggregates';
+import { IEventBase } from '../../events';
+import { Collection } from 'mongoose';
+
+const noop = () => null;
+
+export class MongoRepository<T extends IAggregate> implements IRepository<T> {
+  constructor(
+    private readonly collection: Collection,
+  ) {}
+
+  public async insert(key: string, events: IEventBase[]): Promise<void> {
+    const document: IDocument = await this.collection.findOne({ id: key });
+    if (!!document) {
+      events.forEach((event) => document.events.push(event));
+      await this.collection.updateOne({ id: key }, document);
+    } else {
+      await this.collection.insertOne({
+        _id: key,
+        events,
+      });
+    }
+  }
+
+  public async find(arg1: any, arg2?: any): Promise<T | T[]> {
+    let key: string;
+    let process: (events: IEventBase[]) => T = noop;
+    if (!!arg1 && !!arg2) {
+      key = arg1;
+      process = arg2;
+    } else {
+      process = arg1;
+    }
+    if (!!key) {
+      return process((await this.collection.findOne({ id: key })).events);
+    } else {
+      return (await this.collection.find().toArray())
+        .map((document: IDocument) => process(document.events));
+    }
+  }
+
+}
